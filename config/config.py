2249e99d977eaa6ba9bebf0ce8eadc4717ee9875
Ian Bennett <34078802+ian612@users.noreply.github.com>
2023-08-24 14:00:09 -0400

    add block

--- config/config.py
+++ config/config.py
@@ -55,6 +55,7 @@ start_rotation = 0          # Robot starting rotation (deg)
 robot_width = 6             # Robot width in inches
 robot_height = 6            # Robot height in inches
 block_position = [25, 41]   # Block starting location
+block_rotation = 0          # Block rotation (deg)
 block_size = 3              # Block side length in inches
 
 # Drive information
@@ -86,6 +87,9 @@ wall_color = (255, 0, 0)    # Tuple with wall color in (R,G,B) format
 
 robot_thickness = 0.25      # Thickness to draw robot perimeter, in inches
 robot_color = (0, 0, 255)   # Tuple with robot perimeter color in (R,G,B) format
+
+block_thickness = 0.25      # Thickness to draw robot perimeter, in inches
+block_color = (127, 127, 0) # Tuple with robot perimeter color in (R,G,B) format
 
 
 

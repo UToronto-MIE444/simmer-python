53ac41c10bfc1486687f3b31cddbcf5b9d6741b3
Ian Bennett <34078802+ian612@users.noreply.github.com>
2023-09-07 20:36:59 -0400

    added dead reckoning navigation script

--- config.py
+++ config.py
@@ -44,8 +44,8 @@ block_rotation = 0              # Block rotation (deg)
 block_size = 3                  # Block side length in inches
 
 # Robot information
-robot_start_position = [8, 40]  # Robot starting location (in)
-robot_start_rotation = 0        # Robot starting rotation (deg)
+robot_start_position = [6, 42]  # Robot starting location (in)
+robot_start_rotation = 180      # Robot starting rotation (deg)
 robot_width = 6                 # Robot width in inches
 robot_height = 6                # Robot height in inches
 robot_outline = [               # Robot outline, relative to center position
@@ -112,9 +112,9 @@ w0_info = {
     'velocity': [0, 6],
     'ang_velocity': 0,
     'motors': [motors['m0'], motors['m1']],
-    'motor_direction': [1, 1],
-    'bias': {'x': 0, 'y': 0, 'rotation': 0.5},
-    'error': {'x': 0.02, 'y': 0.05, 'rotation': 1}
+    'motor_direction': [1, 1]
+    #'bias': {'x': 0, 'y': 0, 'rotation': 0.5},
+    #'error': {'x': 0.02, 'y': 0.05, 'rotation': 1}
 }
 
 d0_info = {
@@ -125,9 +125,9 @@ d0_info = {
     'velocity': [6, 0],
     'ang_velocity': 0,
     'motors': [motors['m0'], motors['m1']],
-    'motor_direction': [1, 1],
-    'bias': {'x': 0, 'y': 0, 'rotation': 1},
-    'error': {'x': 0.05, 'y': 0.02, 'rotation': 1}
+    'motor_direction': [1, 1]
+    #'bias': {'x': 0, 'y': 0, 'rotation': 1},
+    #'error': {'x': 0.05, 'y': 0.02, 'rotation': 1}
 }
 
 r0_info = {
@@ -138,9 +138,9 @@ r0_info = {
     'velocity': [0, 0],
     'ang_velocity': 120,
     'motors': [motors['m0'], motors['m1']],
-    'motor_direction': [1, -1],
-    'bias': {'x': 0, 'y': 0, 'rotation': 0.02},
-    'error': {'x': 0.003, 'y': 0.003, 'rotation': 0.02}
+    'motor_direction': [1, -1]
+    #'bias': {'x': 0, 'y': 0, 'rotation': 0.02},
+    #'error': {'x': 0.003, 'y': 0.003, 'rotation': 0.02}
 }
 
 drives = {
---
+++ scripts/dead_reckoning.py
@@ -0,0 +1,98 @@
+'''
+This file is part of SimMeR, an educational mechatronics robotics simulator.
+Initial development funded by the University of Toronto MIE Department.
+Copyright (C) 2023  Ian G. Bennett
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+# Basic echo client, for testing purposes
+# Code modified from examples on https://realpython.com/python-sockets/
+# and https://www.geeksforgeeks.org/python-display-text-to-pygame-window/
+
+import socket
+import struct
+import time
+import math
+from threading import Thread
+import _thread
+from datetime import datetime
+
+def transmit(data):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect((HOST, PORT_TX))
+            s.send(data.encode('utf-8'))
+        except (ConnectionRefusedError, ConnectionResetError):
+            print('Tx Connection was refused or reset.')
+            _thread.interrupt_main()
+        except TimeoutError:
+            print('Tx socket timed out.')
+            _thread.interrupt_main()
+        except EOFError:
+            print('\nKeyboardInterrupt triggered. Closing...')
+            _thread.interrupt_main()
+
+def receive():
+    global responses
+    global time_rx
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s2:
+            try:
+                s2.connect((HOST, PORT_RX))
+                response_raw = s2.recv(1024)
+                if response_raw:
+                    responses = bytes_to_list(response_raw)
+                    time_rx = datetime.now().strftime("%H:%M:%S")
+            except (ConnectionRefusedError, ConnectionResetError):
+                print('Rx connection was refused or reset.')
+                _thread.interrupt_main()
+            except TimeoutError:
+                print('Response not received from robot.')
+                _thread.interrupt_main()
+
+def bytes_to_list(msg):
+    num_responses = int(len(msg)/8)
+    data = struct.unpack("%sd" % str(num_responses), msg)
+    return data
+
+
+### Network Setup ###
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT_TX = 61200     # The port used by the *CLIENT* to receive
+PORT_RX = 61201     # The port used by the *CLIENT* to send data
+
+# Received responses
+responses = [False]
+time_rx = 'Never'
+
+# Create tx and rx threads
+Thread(target = receive, daemon = True).start()
+
+# Run the sequence of commands
+RUNNING = True
+cmd_sequence = ['w0-36', 'r0-90', 'w0-36', 'r0-90', 'w0-12', 'r0--90', 'w0-24', 'r0--90', 'w0-6', 'r0-720']
+
+ct = 0
+while RUNNING:
+
+    if ct < len(cmd_sequence):
+        transmit(cmd_sequence[ct])
+        time.sleep(0.1)
+        if responses[0] == math.inf:
+            ct += 1
+        time.sleep(1)
+    else:
+        RUNNING = False
+        print("Sequence complete!")

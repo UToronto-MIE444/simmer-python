e08b6bdc19deb356f4cca7ad409ace345fd8735c
Rishabh Garikiparithi <rishabh.garikiparithi@mail.utoronto.ca>
2024-01-01 01:02:18 -0500

    feat: simulateFast function for faster sensing

--- devices/ultrasonic.py
+++ devices/ultrasonic.py
@@ -107,6 +107,45 @@ class Ultrasonic(Device):
             # Decrement the buffer
             self.visible_measurement_buffer -= 1
 
+    def simulateFast(self, value: float, environment: dict):
+        """
+        Simulates the performance of an ultrasonic sensor.
+
+        Response data format
+        [0:7] - Eight byte double
+        """
+        MAZE = environment.get("MAZE", False)
+        # BLOCK = environment.get('BLOCK', False)
+
+        rays = self._define_rays()
+        ray_lengths = [self.max_range for item in rays]
+
+        # Update the measurement display buffer
+        self.visible_measurement_buffer = int(self.visible_measurement_time * CONFIG.frame_rate)
+
+        for ct, ray in enumerate(rays):
+            # Check if the sensor is at a height where the block would be seen
+            # if self._block_visible(BLOCK):
+            #     to_check = [BLOCK.block_square, *MAZE.wall_squares]
+            # else:
+
+                for wall in MAZE.reduced_walls:
+                    collision_points = utilities.collision(ray, wall)
+                    if not collision_points:
+                        pass
+                    else:
+                        rays[ct][1], ray_lengths[ct] = utilities.closestFast(
+                            self.position_global, collision_points
+                        )
+
+        # Update stored variables
+        self.rays = rays
+        self.ray_lengths = ray_lengths
+
+        # Build the value to return
+        output = min(self.ray_lengths)
+        
+        return utilities.add_error(output, self.error_pct, self.reading_bounds)
 
     def simulate(self, value: float, environment: dict):
         '''
